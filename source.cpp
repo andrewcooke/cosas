84962db62bf7c324ad05a7a253e83460bc883448
andrew cooke <andrew@acooke.org>
2025-05-12 17:48:02 -0400

    sources are not const because latch has state

---
+++ source.cpp
@@ -0,0 +1,24 @@
+
+import std;
+using namespace std;
+
+#include "source.h"
+
+
+Latch::Latch(Source& s) : source(s) {};
+
+uint16_t Latch::next(int64_t tick, int32_t phi) {
+  if (! on) {
+    On(*this);
+    previous = source.next(tick, phi);
+  }
+  return previous;
+}
+
+On::On(Latch& l) : latch(l) {
+  latch.on = true;
+}
+
+On::~On() {
+  latch.on = false;
+}
