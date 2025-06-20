1ab59cde9350f1ec8e67d2d232434efb35a4f1ed
andrew cooke <andrew@cosas.europa2189>
2025-06-20 14:03:25 -0400

    organising on debian

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
+#git clone https://github.com/raspberrypi/pico-sdk.git
+ln -s pico-sdk/external/pico_sdk_import.cmake .
