78557ce05a6ebe6f70de8a4dea814e9c2f2212b5
andrew cooke <andrew@cosas.europa2189>
2025-06-20 15:51:56 -0400

    get build working

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 export PICO_SDK_PATH=`pwd`/pico-sdk
+
+#rm -fr build
+
 mkdir -p build
 pushd build > /dev/null
 cmake ..
+make
--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
+sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib g++
 git clone https://github.com/raspberrypi/pico-sdk.git
+pushd pico-sdk > /dev/null
+git submodule update --init
+popd
 ln -s pico-sdk/external/pico_sdk_import.cmake .
