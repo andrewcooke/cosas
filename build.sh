80b4036ddead0dd4134cb62babdd2064cd4d07cc
andrew cooke <andrew@acooke.org>
2025-06-23 11:38:32 -0400

    seems ok, but does not actually build anything

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 if [ ! -e pico_sdk_import.cmake ]; then
-    wget -L https://raw.github.com/raspberrypi/pico-sdk/master/pico_sdk_import.cmake
+    wget -L https://raw.github.com/raspberrypi/pico-sdk/master/external/pico_sdk_import.cmake
 fi
 
 mkdir -p build
