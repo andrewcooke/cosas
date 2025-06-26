d3b07dc426ceff98b94e70ca9df5109c24ca9078
andrew cooke <andrew@acooke.org>
2025-06-26 15:51:27 -0400

    compiles!

--- build.sh
+++ build.sh
@@ -2,11 +2,6 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-if [ ! -e pico_sdk_import.cmake ]; then
-    wget -L https://raw.github.com/raspberrypi/pico-sdk/master/external/pico_sdk_import.cmake
-fi
-
-mkdir -p build
 pushd build > /dev/null
-cmake ..
-make
+cmake --build .
+
--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib g++
-git clone https://github.com/raspberrypi/pico-sdk.git
-pushd pico-sdk > /dev/null
-git submodule update --init
-popd
-ln -s pico-sdk/external/pico_sdk_import.cmake .
+
+if [ ! -e pico_sdk_import.cmake ]; then
+    wget -L https://raw.github.com/raspberrypi/pico-sdk/master/external/pico_sdk_import.cmake
+fi
+
+mkdir -p build
+pushd build > /dev/null
+cmake ..
