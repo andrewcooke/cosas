27457f310f7d8c8a460bf60914aedc4c7d354c2d
andrew cooke <andrew@acooke.org>
2025-07-03 15:08:26 -0400

    refactoring before wotking on pico-specific code

--- clean.sh
+++ clean.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -x
 
-#rm -fr pico-sdk
-#rm -f pico_sdk_import.cmake
-#rm -fr Workshop_Computer
-rm -fr CMakeFiles cmake_install.cmake CMakeCache.txt _deps generated pi* Makefile
+#rm -fr CMakeFiles cmake_install.cmake CMakeCache.txt _deps generated pi* Makefile
 rm -f *~
-rm -fr build build-pico build-amd64
+rm -fr build build-pico build-amd64 cmake-build*
+
 
