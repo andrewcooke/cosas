e0a8bc9bb64ff9e8457cf27ef1ee21beba71bebe
andrew cooke <andrew@acooke.org>
2025-07-04 15:34:55 -0400

    clean

--- cosas/include/cosas/constants.h
+++ cosas/include/cosas/constants.h
@@ -13,7 +13,6 @@ constexpr uint8_t SAMPLE_BITS = 16;
 constexpr int16_t SAMPLE_MAX = std::numeric_limits<int16_t>::max();
 constexpr int16_t SAMPLE_MIN = -SAMPLE_MAX;  // throw away one value because i don't like the asymmetry
 
-// leads to omega = f (see source.h)
 constexpr size_t FULL_TABLE_SIZE = SAMPLE_RATE;
 constexpr size_t HALF_TABLE_SIZE = SAMPLE_RATE / 2;
 constexpr size_t QUARTER_TABLE_SIZE = SAMPLE_RATE / 4;
@@ -21,7 +20,6 @@ constexpr size_t QUARTER_TABLE_SIZE = SAMPLE_RATE / 4;
 constexpr uint8_t SUBTICK_BITS = 3;
 constexpr size_t FULL_TABLE_SUB = FULL_TABLE_SIZE << SUBTICK_BITS;
 
-// see discussion in oscillator.cpp
 constexpr size_t PHI_FUDGE_BITS = 12;
 
 
@@ -29,13 +27,10 @@ inline size_t tick2idx(int32_t tick) {
     return (tick >> SUBTICK_BITS) % FULL_TABLE_SIZE;
 }
 
-inline int32_t hz2tick(const float hz) {
-    return static_cast<int32_t>(hz * (1 << SUBTICK_BITS));
-}
-
 inline uint32_t hz2freq(const float hz) {
-    // TODO - min + max
-    return static_cast<uint32_t>(std::fabsf(hz) * (1 << SUBTICK_BITS));
+    auto freq = static_cast<uint32_t>(std::fabsf(hz) * (1 << SUBTICK_BITS));
+    freq = std::min((SAMPLE_RATE / 2) << SUBTICK_BITS, std::max(1u, freq));
+    return freq;
 }
 
 
