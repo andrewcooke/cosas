b62ad29fd2af2b713867fbedb9f1effbd7769e60
andrew cooke <andrew@acooke.org>
2025-07-12 11:49:40 -0400

    prev commit was after losing code; adding back knobs

--- weas/include/weas/codec.h
+++ weas/include/weas/codec.h
@@ -9,29 +9,38 @@
 
 class Codec {
 
+public:
+  Codec(const Codec&) = delete;
+  Codec& operator=(const Codec&) = delete;
+  static Codec& get();
+  enum Knob {Main, X, Y};
+  enum Switch {Down, Middle, Up};
+
 private:
+  static constexpr uint PLS_IN = 2;
+  static constexpr uint PLS_OUT = 8;
   static constexpr uint DAC_SCK = 18;
   static constexpr uint DAC_TX = 19;
   static constexpr uint DAC_CS = 21;
+  static constexpr uint CV_OUT = 22;
   static constexpr uint MUX_OUT = 24;
   static constexpr uint ADC_IN = 26;
   static constexpr uint MUX_IN = 28;
   static constexpr float SAMPLE_FREQ = 44100;
   static constexpr uint OVERSAMPLE = 2;
   Codec();
-  uint32_t count = 0;
-  uint8_t adc_dma, spi_dma;
-  uint16_t adc_buffer[2][4 * OVERSAMPLE], spi_buffer[2][2];  // [phase][l/r/over]
-  uint cpu_phase = 1, dma_phase = 0;  // alternating indices into buffers
-  volatile int16_t cv[2] = {0, 0};
-  volatile int16_t adc[2] = {0x800, 0x800};
-  volatile bool pulse[2][2];  // [p/c][l/r]
-
-public:
-  Codec(const Codec&) = delete;
-  Codec& operator=(const Codec&) = delete;
-  static Codec& get();
   void adc_callback();
+  uint32_t count = 0;
+  uint8_t adc_dma, dac_dma;
+  uint16_t adc_buffer[2][4 * OVERSAMPLE] = {};  // [phase][over]
+  uint16_t spi_buffer[2][2] = {};  // [phase][l/r]
+  uint cpu_phase = 1, dma_phase = 0;  // alternating indices into buffers
+  volatile int16_t cv[2] = {};
+  volatile int16_t adc[2] = {0x800, 0x800};
+  volatile bool pulse[2][2] = {};  // [p/c][l/r]
+  volatile Switch switch_[2] = {};
+  volatile uint16_t knobs[2][4] = {};
+
 };
 
 
--- weas/src/codec.cpp
+++ weas/src/codec.cpp
@@ -19,16 +19,25 @@ void callback() {
 
 Codec::Codec() {
 
+  for (uint lr = 0; lr < 2; lr++) {
+    gpio_init(PLS_OUT + lr);
+    gpio_set_dir(PLS_OUT + lr, GPIO_OUT);
+    gpio_put(PLS_OUT + lr, true);
+    gpio_init(PLS_IN + lr);
+    gpio_set_dir(PLS_IN+ lr, GPIO_IN);
+    gpio_pull_up(PLS_IN + lr);
+  }
+
   spi_init(spi0, 15625000);  // why this baudrate?  max is 20MHz
   spi_set_format(spi0, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
   gpio_set_function(DAC_SCK, GPIO_FUNC_SPI);
   gpio_set_function(DAC_TX, GPIO_FUNC_SPI);
   gpio_set_function(DAC_CS, GPIO_FUNC_SPI);
-  spi_dma = dma_claim_unused_channel(true);  // DAC (maybe change name?)
-  dma_channel_config spi_dmacfg = dma_channel_get_default_config(spi_dma);
+  dac_dma = dma_claim_unused_channel(true);
+  dma_channel_config spi_dmacfg = dma_channel_get_default_config(dac_dma);
   channel_config_set_transfer_data_size(&spi_dmacfg, DMA_SIZE_16);
   channel_config_set_dreq(&spi_dmacfg, DREQ_SPI0_TX);
-  dma_channel_configure(spi_dma, &spi_dmacfg, &spi_get_hw(spi0)->dr, NULL, 2, false);
+  dma_channel_configure(dac_dma, &spi_dmacfg, &spi_get_hw(spi0)->dr, NULL, 2, false);
 
   for (uint mux = 0; mux < 2; mux++) {
     gpio_init(MUX_OUT + mux);
@@ -49,6 +58,7 @@ Codec::Codec() {
   channel_config_set_dreq(&adc_dmacfg, DREQ_ADC);
   dma_channel_configure(adc_dma, &adc_dmacfg, adc_buffer[dma_phase], &adc_hw->fifo, 8, true);
   dma_channel_set_irq0_enabled(adc_dma, true);
+
   irq_set_enabled(DMA_IRQ_0, true);
   irq_set_exclusive_handler(DMA_IRQ_0, callback);
 }
@@ -68,15 +78,27 @@ void Codec::adc_callback() {
   cpu_phase = dma_phase;
   dma_phase = 1 - dma_phase;
   dma_channel_set_write_addr(adc_dma, adc_buffer[dma_phase], true);
-  dma_channel_set_read_addr(spi_dma, spi_buffer[dma_phase], true);
+  dma_channel_set_read_addr(dac_dma, spi_buffer[dma_phase], true);
 
   uint cv_idx = count % 2;
   // this almost fits in 16 bits, but i guess there's no advantage in pushing it to do so?
   cv_smooth[cv_idx] = (15 * (cv_smooth[cv_idx]) + 16 * fix_dnl(adc_buffer[cpu_phase][3])) >> 4;
   cv[cv_idx] = static_cast<int16_t>(2048 - cv_smooth[cv_idx]);  // cc has an extra >> 4 here i don't understand
 
-  for (uint lr = 0; lr < 2; lr++) adc[lr] = -(fix_dnl((adc_buffer[cpu_phase][0+lr] + adc_buffer[cpu_phase][4+lr]) >> 1) - 0x1000);
+  for (uint lr = 0; lr < 2; lr++) {
+    adc[lr] = -(fix_dnl((adc_buffer[cpu_phase][0+lr] + adc_buffer[cpu_phase][4+lr]) >> 1) - 0x1000);
+    pulse[1][lr] = pulse[0][lr];
+    pulse[0][lr] = !gpio_get(PLS_IN);
+  }
 
+  const uint knob = count & 0x3;
+  knobs_smooth[knob] = (127 * (knobs_smooth[knob]) + 16 * adc_buffer[cpu_phase][6]) >> 7;
+  knobs[1][knob] = knobs[0][knob];
+  knobs[0][knob] = knobs_smooth[knob];  // extra >> 4 here
+  if (knob == 3) {
+    switch_[1] = switch_[0];
+    switch_[0] = static_cast<Switch>((knobs[0][knob]>1000) + (knobs[0][knob]>3000));
+  }
 
   count++;
   for (uint mux = 0; mux < 2; mux++) gpio_put(MUX_OUT + mux, count & (0b1 << mux));
